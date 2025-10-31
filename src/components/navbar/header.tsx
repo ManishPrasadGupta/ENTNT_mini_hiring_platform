042774ab970a14420af258873549730066a9c996
Manish Prasad Gupta <manishprasad7173@gmail.com>
2025-10-31 12:16:54 +0530

    fix: correct background gradient class in navbar component

--- src/components/navbar/header.tsx
+++ src/components/navbar/header.tsx
@@ -7,7 +7,7 @@ export default function Header({
   onSidebarOpen: () => void;
 }) {
   return (
-    <nav className="fixed top-0 left-0 w-full h-14 flex items-center justify-between px-6 bg-gradient-to-b from-emerald-50 to-white/90 border-b shadow-md backdrop-blur z-50">
+    <nav className="fixed top-0 left-0 w-full h-14 flex items-center justify-between px-6 bg-linear-to-b from-emerald-50 to-white/90 border-b shadow-md backdrop-blur z-50">
       <div className="flex items-center space-x-3">
         <button
           type="button"
